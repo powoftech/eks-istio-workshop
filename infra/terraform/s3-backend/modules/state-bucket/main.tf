4be65ffdf8a2cb37645899e84bced86258a87868
Phuong Dang <powoftech@gmail.com>
2025-08-11 23:13:03 +0700

    fix: remove deprecated DynamoDB table in S3 backend

--- infra/terraform/s3-backend/modules/state-bucket/main.tf
+++ infra/terraform/s3-backend/modules/state-bucket/main.tf
@@ -27,14 +27,3 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_dynamodb_table" "tofu_locks" {
-  name         = var.name
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
--- infra/terraform/s3-backend/outputs.tf
+++ infra/terraform/s3-backend/outputs.tf
@@ -2,8 +2,3 @@ output "s3_bucket_name" {
   description = "The name of the S3 bucket"
   value       = module.state_bucket.s3_bucket_name
 }
-
-output "dynamodb_table_name" {
-  description = "The name of the DynamoDB table for state locking"
-  value       = module.state_bucket.dynamodb_table_name
-}

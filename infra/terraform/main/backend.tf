d1c52bf0273d8b84a7812600cd94aa92ab2477bf
Phuong Dang <powoftech@gmail.com>
2025-08-14 04:45:29 +0700

    BREAKING CHANGE: reset the project

--- infra/terraform/main/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-593793056386-us-east-2"
-    key          = "eks-istio-workshop/infra/main/terraform.tfstate"
-    region       = "us-east-2"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
--- infra/terraform/s3-backend/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = module.state_bucket.s3_bucket_name
-}
